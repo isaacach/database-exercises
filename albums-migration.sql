0fac8261b429c74d81583267a07cc84d29a2a289
isaacachenbach <isaac.achenbach1@gmail.com>
2023-05-01 15:09:11 -0500

    updated database exercise

--- albums-migration.sql
+++ albums-migration.sql
@@ -1,4 +1,4 @@
-USE bigfoot_test_db;
+USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
@@ -6,7 +6,7 @@ CREATE TABLE albums (
                        user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                        artist VARCHAR(65) NOT NULL,
                        name VARCHAR(65) NOT NULL,
-                       release_date DATE NOT NULL,
+                       release_date int NOT NULL,
                        sales FLOAT NOT NULL,
                        genre VARCHAR(65) NOT NULL,
                        PRIMARY KEY (user_id)
