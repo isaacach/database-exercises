bef965f88d7b08e5af78fd2fb40098c120f82226
isaacachenbach <isaac.achenbach1@gmail.com>
2023-05-02 21:55:43 -0500

    finished databases

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -5,3 +5,13 @@ DELETE FROM albums WHERE release_date > 1991;
 DELETE FROM albums WHERE genre = 'Disco';
 
 DELETE FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE release_date > 1991;
+
+DELETE FROM albums WHERE genre = 'Disco';
+
+DELETE FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE release_date > 1991;
+
+DELETE FROM albums WHERE genre = 'Disco';
+
+DELETE FROM albums WHERE artist = 'Whitney Houston';
